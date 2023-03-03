b9d95c1a83edbfd8264323a54e828b3267beeaf0
metya <metya.tm@gmail.com>
2023-03-03 02:16:58 +0000

    fix: fix backup

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -1,4 +1,4 @@
 cd /app
 git add papers.db
 git commit -m "backup papers.db" --no-verify
-expect scripts/script.exp ${GITHUB_TOKEN}
+git push https://metya:{GITHUB_TOKEN}@github.com/metya/vanity-bot.git
